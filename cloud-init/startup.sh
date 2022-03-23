66596054f42b4dff2bff1b2102339df2598a538b
Joshua Post <joshpost@hotmail.com>
2022-03-23 17:53:25 -0500

    Correct startup script

--- cloud-init/startup.sh
+++ cloud-init/startup.sh
@@ -15,14 +15,14 @@ git clone -b release https://github.com/TheSpaghettiDetective/TheSpaghettiDetect
 # Attempt to restore a backup if present
 bucket=$(curl -L http://169.254.169.254/opc/v1/instance/metadata | jq -c ".bucket" | tr --delete '"')
 if [ ${bucket} ]; then
-# Download existing database backup
-sudo wget ${bucket}db.sqlite3 -O /TheSpaghettiDetective/web/db.sqlite3
-sudo wget ${bucket}docker-compose.yml -O /TheSpaghettiDetective/docker-compose.yml
+	# Download existing database backup
+	sudo wget ${bucket}db.sqlite3 -O /TheSpaghettiDetective/web/db.sqlite3
+	sudo wget ${bucket}docker-compose.yml -O /TheSpaghettiDetective/docker-compose.yml
 
 
-# Setup daily backups
+	# Setup daily backups
 
-# Create backup script file
+	# Create backup script file
 	echo '#!/bin/bash' | sudo tee /TheSpaghettiDetective/tsd-backup.sh
 	echo 'bucket=$(curl -L http://169.254.169.254/opc/v1/instance/metadata | jq -c ".bucket" | tr --delete '\''"'\'' )' | sudo tee -a /TheSpaghettiDetective/tsd-backup.sh
 	# echo sudo zip -r /tmp/TheSpaghettiDetectiveBackup.zip /TheSpaghettiDetective/web | sudo tee -a /TheSpaghettiDetective/tsd-backup.sh
@@ -31,14 +31,14 @@ sudo wget ${bucket}docker-compose.yml -O /TheSpaghettiDetective/docker-compose.y
 	echo curl -T /TheSpaghettiDetective/docker-compose.yml \$bucket | sudo tee -a /TheSpaghettiDetective/tsd-backup.sh
 	sudo chmod u+x /TheSpaghettiDetective/tsd-backup.sh
 
-# Schedule weekly backups of database using cron on Sundays at 1:00 am
-# https://stackoverflow.com/questions/878600/how-to-create-a-cron-job-using-bash-automatically-without-the-interactive-editor
-crontab -l | { cat; echo "* 1 * * 0 /TheSpaghettiDetective/tsd-backup.sh"; } | crontab -
+	# Schedule weekly backups of database using cron on Sundays at 1:00 am
+	# https://stackoverflow.com/questions/878600/how-to-create-a-cron-job-using-bash-automatically-without-the-interactive-editor
+	crontab -l | { cat; echo "* 1 * * 0 /TheSpaghettiDetective/tsd-backup.sh"; } | crontab -
+fi
 
 
 # Build and start TheSpaghettiDetective
 cd /TheSpaghettiDetective && sudo docker-compose up -d
-
 
 # Install all remaining updates and keep current iptables settings
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
